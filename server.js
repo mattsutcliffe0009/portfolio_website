cc66c13f15e76cd46c846c6459b757929a7d9b76
msut0009 <mattsutcliffe0009@gmail.com>
2026-02-01 20:08:44 +1100

    little fixes

--- server.js
+++ server.js
@@ -11,16 +11,15 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get("/projects/ratbot", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/projects/ratbot.html"));
+
+
+app.get("/construction", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/construction.html"));
 });
 
-app.get("/projects/distortion-pedal", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/projects/distortion_pedal.html"));
-});
+app.get("/projects/ratbot", (req, res) => res.redirect("/construction"));
+app.get("/projects/distortion-pedal", (req, res) => res.redirect("/construction"));
+app.get("/projects/self-hosted-website", (req, res) => res.redirect("/construction"));
 
-app.get("/projects/self-hosted-website", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/projects/website.html"));
-});
 
 app.listen(5000, () => console.log("http://localhost:5000"));
